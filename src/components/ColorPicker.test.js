8b2df8627ccb611af54c35f279b4ddf876c6cf09
dreamer <wildetangeant@gmail.com>
2019-05-14 11:02:03 -0700

    simple color picker display did

---
+++ src/components/ColorPicker.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ColorPicker from './ColorPicker';
+
+describe('ColorPicker component', () => {
+  it('renders a ColorPicker', () => {
+    const wrapper = shallow(<ColorPicker />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('changes a color to red on button press', () => {
+    const wrapper = shallow(<ColorPicker />);
+    wrapper.find('button').at(0).simulate('click');
+
+    expect(wrapper.state('color')).toEqual('red');
+  });
+
+
+})
+;
